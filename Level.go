08d1510b43c7b1e6d2779745aa7ffdc312026e8a
Christian Haas <christian.haas@sevensuns.at>
2015-12-25 17:42:25 +0100

    started with level objects added changing of floor texture

--- Level.go
+++ Level.go
@@ -3,6 +3,7 @@ package core
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"sync"
 
 	"github.com/inkyblackness/res"
@@ -56,6 +57,9 @@ type Level struct {
 
 	tileMapStore chunk.BlockStore
 	tileMap      []data.TileMapEntry
+
+	objectListStore chunk.BlockStore
+	objectList      []data.LevelObjectEntry
 }
 
 func NewLevel(store chunk.Store, id int) *Level {
@@ -80,6 +84,18 @@ func (level *Level) onTileDataChanged() {
 
 	binary.Write(buf, binary.LittleEndian, &level.tileMap)
 	level.tileMapStore.SetBlockData(0, buf.Bytes())
+}
+
+func (level *Level) bufferObjectList() []data.LevelObjectEntry {
+	if level.objectList == nil {
+		level.objectListStore = level.store.Get(res.ResourceID(4000 + level.id*100 + 8))
+		blockData := level.objectListStore.BlockData(0)
+		level.objectList = make([]data.LevelObjectEntry, len(blockData)/data.LevelObjectEntrySize)
+		reader := bytes.NewReader(blockData)
+		binary.Read(reader, binary.LittleEndian, &level.objectList)
+	}
+
+	return level.objectList
 }
 
 func (level *Level) ID() int {
@@ -111,6 +127,47 @@ func (level *Level) Textures() (result []int) {
 	return
 }
 
+func bytesToIntAray(bs []byte) []int {
+	result := make([]int, len(bs))
+	for index, value := range bs {
+		result[index] = int(value)
+	}
+
+	return result
+}
+
+func (level *Level) Objects() []model.LevelObject {
+	level.mutex.Lock()
+	defer level.mutex.Unlock()
+
+	var result []model.LevelObject
+	entries := level.bufferObjectList()
+
+	for index, rawEntry := range entries {
+		if rawEntry.IsInUse() {
+			entry := model.LevelObject{
+				Identifiable: model.Identifiable{ID: fmt.Sprintf("%d", index)},
+				Class:        int(rawEntry.Class),
+				Subclass:     int(rawEntry.Subclass),
+				Type:         int(rawEntry.Type)}
+
+			entry.BaseProperties.TileX = int(rawEntry.X >> 8)
+			entry.BaseProperties.FineX = int(rawEntry.X & 0xFF)
+			entry.BaseProperties.TileY = int(rawEntry.Y >> 8)
+			entry.BaseProperties.FineY = int(rawEntry.Y & 0xFF)
+			entry.BaseProperties.Z = int(rawEntry.Z)
+
+			entry.Hacking.Unknown0013 = bytesToIntAray(rawEntry.Unknown0013[:])
+			entry.Hacking.Unknown0015 = bytesToIntAray(rawEntry.Unknown0015[:])
+			entry.Hacking.Unknown0017 = bytesToIntAray(rawEntry.Unknown0017[:])
+
+			result = append(result, entry)
+		}
+	}
+
+	return result
+}
+
 func (level *Level) TileProperties(x, y int) (result model.TileProperties) {
 	level.mutex.Lock()
 	defer level.mutex.Unlock()
@@ -131,11 +188,16 @@ func (level *Level) TileProperties(x, y int) (result model.TileProperties) {
 		var properties model.RealWorldTileProperties
 		var textureIDs = uint16(entry.Textures)
 
-		properties.WallTexture = int(textureIDs & 0x3F)
-		properties.CeilingTexture = int((textureIDs >> 6) & 0x1F)
-		properties.CeilingTextureRotations = int((entry.Ceiling >> 5) & 0x03)
-		properties.FloorTexture = int((textureIDs >> 11) & 0x1F)
-		properties.FloorTextureRotations = int((entry.Floor >> 5) & 0x03)
+		properties.WallTexture = new(int)
+		*properties.WallTexture = int(textureIDs & 0x3F)
+		properties.CeilingTexture = new(int)
+		*properties.CeilingTexture = int((textureIDs >> 6) & 0x1F)
+		properties.CeilingTextureRotations = new(int)
+		*properties.CeilingTextureRotations = int((entry.Ceiling >> 5) & 0x03)
+		properties.FloorTexture = new(int)
+		*properties.FloorTexture = int((textureIDs >> 11) & 0x1F)
+		properties.FloorTextureRotations = new(int)
+		*properties.FloorTextureRotations = int((entry.Floor >> 5) & 0x03)
 		result.RealWorld = &properties
 	}
 
@@ -160,6 +222,15 @@ func (level *Level) SetTileProperties(x, y int, properties model.TileProperties)
 	}
 	if properties.SlopeHeight != nil {
 		entry.SlopeHeight = byte(*properties.SlopeHeight)
+	}
+	if properties.RealWorld != nil {
+		var textureIDs = uint16(entry.Textures)
+
+		if properties.RealWorld.FloorTexture != nil {
+			textureIDs = (textureIDs & 0x01FF) | (uint16(*properties.RealWorld.FloorTexture) << 11)
+		}
+
+		entry.Textures = data.TileTextureInfo(textureIDs)
 	}
 
 	level.onTileDataChanged()
